a9d65ef4da31601534f5560173a92b5521e49d21
Shaheer Mirza <shaheermirzacs@gmail.com>
2020-11-09 01:43:24 -0600

    database issues

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,7 +4,6 @@ from django.db import models, IntegrityError
 from django.contrib.auth.models import User
 
 class Reservation(models.Model):
-    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     email = models.EmailField()
     datetime = models.DateTimeField()
