a19491ffd694f51852589d89b3bf0b59c0af8d1a
Shaheer Mirza <shaheermirzacs@gmail.com>
2020-11-09 00:07:19 -0600

    restart

--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-"""solsticesociety URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,17 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from solsticesociety.api import views
-from .views import index
+from django.urls import path
 
-router = routers.DefaultRouter()
-router.register(r'reservations', views.ReservationViewSet)
+from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
-    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
