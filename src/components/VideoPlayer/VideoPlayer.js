8306cb46add8ea048883d48af377aa8b20f07d80
HumzaBaig <humzacodemail@gmail.com>
2020-12-06 16:47:03 -0600

    change margin for main-content in App.css as well as dangerouslySetInnerHTML for video again for mobile video to autoplay inline

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -4,28 +4,28 @@ import React from "react";
 const Video = () => {
 
   return (
-    // <div className="video-container" dangerouslySetInnerHTML={{ __html: `
-    //   <video
-    //     loop
-    //     muted
-    //     autoplay
-    //     playsinline
-    //     src="https://res.cloudinary.com/dzixj0ktk/video/upload/ac_none,c_crop,g_center,h_352,q_100,w_300/v1605808078/IMG_2968_eozlvg.mp4"
-    //     class="video"
-    //   />
-    // ` }}>
-    // </div>
-
-    <div className="video-container">
+    <div className="video-container" dangerouslySetInnerHTML={{ __html: `
       <video
-           loop
-           muted
-           autoPlay
-           playsInline
-           src="https://res.cloudinary.com/dzixj0ktk/video/upload/v1605808078/IMG_2968_eozlvg.mp4"
-           className="video"
-         />
+        loop
+        muted
+        autoplay
+        playsinline
+        src="https://res.cloudinary.com/dzixj0ktk/video/upload/v1605808078/IMG_2968_eozlvg.mp4"
+        class="video"
+      />
+    ` }}>
     </div>
+
+    // <div className="video-container">
+    //   <video
+    //        loop
+    //        muted
+    //        autoPlay
+    //        playsInline
+    //        src="https://res.cloudinary.com/dzixj0ktk/video/upload/ac_none,c_crop,g_center,h_352,q_100,w_300/v1605808078/IMG_2968_eozlvg.mp4"
+    //        className="video"
+    //      />
+    // </div>
   );
 }
 
