925ef90b55e7fa034b76bbd68cf9a9af8a160012
HumzaBaig <humzacodemail@gmail.com>
2020-12-06 16:09:57 -0600

    change video from dangerously setting html to test if the video shows up on mobile now

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -4,16 +4,27 @@ import React from "react";
 const Video = () => {
 
   return (
-    <div className="video-container" dangerouslySetInnerHTML={{ __html: `
+    // <div className="video-container" dangerouslySetInnerHTML={{ __html: `
+    //   <video
+    //     loop
+    //     muted
+    //     autoplay
+    //     playsinline
+    //     src="https://res.cloudinary.com/dzixj0ktk/video/upload/ac_none,c_crop,g_center,h_352,q_100,w_300/v1605808078/IMG_2968_eozlvg.mp4"
+    //     class="video"
+    //   />
+    // ` }}>
+    // </div>
+
+    <div className="video-container">
       <video
-        loop
-        muted
-        autoplay
-        playsinline
-        src="https://res.cloudinary.com/dzixj0ktk/video/upload/ac_none,c_crop,g_center,h_352,q_100,w_300/v1605808078/IMG_2968_eozlvg.mp4"
-        class="video"
-      />
-    ` }}>
+           loop
+           muted
+           autoPlay
+           playsInline
+           src="https://res.cloudinary.com/dzixj0ktk/video/upload/ac_none,c_crop,g_center,h_352,q_100,w_300/v1605808078/IMG_2968_eozlvg.mp4"
+           className="video"
+         />
     </div>
   );
 }
